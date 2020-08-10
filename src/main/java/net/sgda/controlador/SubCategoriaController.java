9c6c7b5d9c17659d2761602008aa329a18871f24
Ajerhy <34432038+Ajerhy@users.noreply.github.com>
2020-08-10 11:04:47 -0400

    xml

--- src/main/java/net/sgda/controlador/SubCategoriaController.java
+++ src/main/java/net/sgda/controlador/SubCategoriaController.java
@@ -7,10 +7,12 @@ import net.sgda.servicio.ICategoriaServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
@@ -47,14 +49,17 @@ public class SubCategoriaController {
     }
     
     @PostMapping("/guardar")
-    public String GuardarCategoria(Categoria categoria,Model model){
+    public String GuardarCategoria(Categoria categoria,BindingResult result/*,Model model*/,RedirectAttributes attributes){
         Mensaje="Sub Categoria Guardado Exitosamente";
-        model.addAttribute("titulo","Listar Sub Categoria");
-        model.addAttribute("link","/sub/categoria/listar");
-        model.addAttribute("mesaje",Mensaje);
-        
+        //model.addAttribute("titulo","Listar Sub Categoria");
+        //model.addAttribute("link","/sub/categoria/listar");
+        //model.addAttribute("mesaje",Mensaje);
+        if (result.hasErrors()) {
+            return Formulario;
+        }
         servicioCategoria.guardar(categoria);
         System.out.println("Categorias:"+categoria);
+        attributes.addFlashAttribute("msg", Mensaje);
         
         return "redirect:/sub/categoria/listar";
     }
